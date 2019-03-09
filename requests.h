2c3233f491a63b396cfa8fa1146d40e664204896
Daniil <arjentix@gmail.com>
2019-03-09 14:32:18 +0300

    Added requests.h now not symlink

--- requests.h
+++ requests.h
@@ -1,1 +1,19 @@
-Glimb-Client/requests.h
+/*
+* Polyakov Daniil
+* Mail: arjentix@gmail.com
+* Github: Arjentix
+* Date: 09.03.19
+*/
+
+#ifndef REQUESTS_H
+#define REQUESTS_H
+
+/*
+* This file describes kinds of requests to the server.
+* Only this requests should be used in the program.
+*/
+
+#define REQUEST_LOGIN "login"
+#define REQUEST_REGISTRATION "registration"
+
+#endif // REQUESTS_H
