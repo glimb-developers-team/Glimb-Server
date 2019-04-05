fa9cd8950626f02134046b23b2ce13724a222275
Daniil <arjentix@gmail.com>
2019-04-05 11:52:29 +0300

    Added material content

---
+++ database/fill_material.sql
@@ -0,0 +1,8 @@
+--
+-- Fill the table material
+--
+
+INSERT INTO material VALUES	("plate", "sq.m", 12.5),
+				("wood boards 20x146x3000mm", "PCs", 4),
+				("plaster", "cb.m", 5.5),
+				("brick", "PCs", 0.24);
