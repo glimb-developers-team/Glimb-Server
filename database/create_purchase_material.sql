50cb5f81063e0b311c5624d6467e986638fa8cc8
Daniil <arjentix@gmail.com>
2019-04-24 21:55:31 +0300

    Fixed typo

--- database/create_purchase_material.sql
+++ database/create_purchase_material.sql
@@ -5,7 +5,7 @@
 CREATE TABLE purchase_material (
 	PurchaseId INT NOT NULL,
 	MaterialTitle VARCHAR(32) NOT NULL,
-	Cuantity INT,
+	Quantity INT,
 	FOREIGN KEY (PurchaseId) REFERENCES purchase (Id),
 	FOREIGN KEY (MaterialTitle) REFERENCES material (Title),
 	PRIMARY KEY (PurchaseId, MaterialTitle)
