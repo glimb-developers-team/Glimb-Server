c208077920e683cb57bb14aa265f8761d87f8b7c
Daniil <arjentix@gmail.com>
2019-05-26 14:17:43 +0300

    Added new column in purchase table

--- database/create_purchase.sql
+++ database/create_purchase.sql
@@ -6,6 +6,7 @@ CREATE TABLE purchase (
 	Id INT AUTO_INCREMENT NOT NULL,
 	ClientNum VARCHAR(16) NOT NULL,
 	ForemanNum VARCHAR(16) NOT NULL,
+	LocalId INT NOT NULL,
 	Status BOOLEAN DEFAULT NULL,
 	FOREIGN KEY (ClientNum) REFERENCES user (PhoneNumber),
 	FOREIGN KEY (ForemanNum) REFERENCES user (PhoneNumber),
