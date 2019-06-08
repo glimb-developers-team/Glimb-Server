1a731300deb5ea9fef7da38e6c32dcdb2cfd5cdf
Daniil <arjentix@gmail.com>
2019-06-08 13:57:44 +0300

    Added DbQuery.h

---
+++ src/DbConnector/DbQuery.h
@@ -0,0 +1,16 @@
+/*
+* author: Polyakov Daniil
+* Mail: arjentix@gmail.com
+* Github: Arjentix
+* Date: 07.06.19
+*/
+
+namespace DbConnector {
+	enum DbQuery {
+		DB_QUERY_REGISTRATION,
+		DB_QUERY_LOGIN,
+		DB_QUERY_GET_MATERIALS,
+		DB_QUERY_STORE_PURCHASE,
+		DB_QUERY_GET_PURCHASES
+	};
+}
