8c945bcb43eb6f21f01454bd923179184ff56092
Daniil <arjentix@gmail.com>
2019-06-08 19:39:43 +0300

    Added DbQueryHandlerBase class

---
+++ src/DbConnector/DbQueryHandlers/DbQueryHandlerBase.cpp
@@ -0,0 +1,78 @@
+/*
+* author: Polyakov Daniil
+* Mail: arjentix@gmail.com
+* Github: Arjentix
+* Date: 08.06.19
+*/
+
+#include "DbQueryHandlerBase.h"
+#include "LogPrinter.h"
+
+#define HOST		"localhost"
+#define USER_NAME	"GlimbAdmin"
+#define PASSWORD	"secret"
+#define DB_NAME		"GlimbRu"
+
+#define QUERY_SIZE	1024
+
+using namespace DbConnector::DbQueryHandler;
+
+MYSQL *DbQueryHandlerBase::_conn_ptr = mysql_init(NULL);
+
+DbQueryHandlerBase::DbQueryHandlerBase()
+{
+	char query[QUERY_SIZE];
+
+	if (_conn_ptr == NULL) {
+		LogPrinter::print(mysql_error(_conn_ptr));
+		throw std::runtime_error( "MySQL initialization failed");
+	}
+
+	_conn_ptr = mysql_real_connect(_conn_ptr, HOST, USER_NAME, PASSWORD, DB_NAME, 0, NULL, 0);
+	if (_conn_ptr == NULL) {
+		LogPrinter::print(mysql_error(_conn_ptr));
+		throw std::runtime_error( "MySQL connection failed");
+	}
+
+	snprintf(query, QUERY_SIZE, "SET NAMES \'CP1251\'");
+	mysql_query(_conn_ptr, query);
+
+	LogPrinter::print("Successfully connected to the MySQL server");
+}
+
+DbQueryHandlerBase::~DbQueryHandlerBase()
+{
+	mysql_close(_conn_ptr);
+	LogPrinter::print("Disconnected from the MySQL server");
+}
+
+int DbQueryHandlerBase::_mysql_query(const char *query)
+{
+	return mysql_query(_conn_ptr, query);
+}
+
+MYSQL_RES* DbQueryHandlerBase::_mysql_use_result()
+{
+	return mysql_use_result(_conn_ptr);
+}
+
+MYSQL_ROW DbQueryHandlerBase::_mysql_fetch_row(MYSQL_RES *mysql_res)
+{
+	return mysql_fetch_row(mysql_res);
+}
+
+void DbQueryHandlerBase::_mysql_free_result(MYSQL_RES *mysql_res)
+{
+	mysql_free_result(mysql_res);
+}
+
+void DbQueryHandlerBase::_check_for_error(int res)
+{
+	char error[QUERY_SIZE];
+
+	if (res != 0) {	// MySQL error
+		snprintf(error, QUERY_SIZE, "MySQL error: %s", mysql_error(_conn_ptr));
+		LogPrinter::print(error);
+		throw std::runtime_error("MySQL query failed");
+	}
+}
---
+++ src/DbConnector/DbQueryHandlers/DbQueryHandlerBase.h
@@ -0,0 +1,54 @@
+/*
+* author: Polyakov Daniil
+* Mail: arjentix@gmail.com
+* Github: Arjentix
+* Date: 08.06.19
+*/
+
+#include "DbArg.h"
+#include "DbResult.h"
+#include "mysql.h"
+
+namespace DbConnector::DbQueryHandler {
+	/*
+	* DbQueryHandlerBase - base abstract class for db query handlers.
+	* Every handler should be extended from this class and should
+	* override handle() method.
+	*/
+	class DbQueryHandlerBase {
+	private:
+		static MYSQL *_conn_ptr;
+
+	public:
+		DbQueryHandlerBase();
+		~DbQueryHandlerBase();
+
+		/*
+		* handle() - main method of handler. Should be overrided
+		* in every child class.
+		* As an real argument should be expected struct extended
+		* from DbArg::DbArg, where all needed parameters can be added.
+		* All DbArg child classes should be places in DbArgs directory.
+		* All the above is similarly usable for method result.
+		*/
+		virtual DbResult::DbResult handle(DbArg::DbArg arg) = 0;
+
+	protected:
+		/*
+		* Here are some wrappers of MySQL API function.
+		* They are needed to limit access to the _conn_ptr variable.
+		* It is enough for getting all wanted data from Db.
+		*/
+		int _mysql_query(const char *query);
+		MYSQL_RES* _mysql_use_result();
+		MYSQL_ROW _mysql_fetch_row(MYSQL_RES *mysql_res);
+		void _mysql_free_result(MYSQL_RES *mysql_res);
+
+		/*
+		* _check_for_error() - checks res from MySQL API for error
+		* and throws exception of type std::runtime_error if there was
+		* an error.
+		*/
+		void _check_for_error(int res);
+	};
+}
